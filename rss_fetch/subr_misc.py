3b03fb45ef8b8da24f308b9ab0d35281dbf0b8e1
Simone Basso <bassosimone@gmail.com>
2013-05-28 10:09:33 +0200

    add rss_fetch: tool to fetch posts from RSS feeds

---
+++ rss_fetch/subr_misc.py
@@ -0,0 +1,63 @@
+# rss_fetch/subr_misc.py
+
+#
+# Copyright (c) 2013 Simone Basso <bassosimone@gmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+""" Miscellaneous subroutines """
+
+import collections
+import logging
+import os
+
+def time_to_folder(date):
+    """ Convert struct time to folder name """
+    if date[2] <= 15:
+        month = date[1]
+        day = 15
+    else:
+        month = date[1] + 1
+        day = 1
+    return os.sep.join([
+                        "b", "post",
+                        "%02d-%02d-%02d" % (date[0], month, day),
+                       ])
+
+def mkdir_recursive_idempotent(path):
+    """ Create the directories indicated by path (idempotent) """
+    pathv = path.split(os.sep)
+    pathv = collections.deque(pathv)
+    curpath = ""
+    while pathv:
+        entry = pathv.popleft()
+        if not entry:
+            continue
+        if curpath:
+            curpath += os.sep
+        curpath += entry
+        if os.path.exists(curpath):
+            if not os.path.isdir(curpath):
+                logging.warning("subr_misc: not a directory")
+                return False
+            continue
+        os.mkdir(curpath, 0755)
+    if not os.path.isdir(path):
+        logging.warning("subr_misc: internal error")
+        return False
+    return True
+
+def bitlink_to_filename(bitlink):
+    """ Convert bitlink to filename """
+    return bitlink.replace("http://bit.ly/", "") + ".html"
---
+++ rss_fetch/subr_rss.py
@@ -0,0 +1,92 @@
+# rss_fetch/subr_rss.py
+
+#
+# Copyright (c) 2013 Simone Basso <bassosimone@gmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+""" Fetch the RSS feeds of a website """
+
+import logging
+import sys
+import urlparse
+
+if __name__ == "__main__":
+    sys.path.insert(0, ".")
+
+from rss_fetch import subr_http
+
+PATHS = (
+         "/rss",      # tumblr.com
+         "/feed/",    # wordpress.com
+         "/rss.xml",  # blogspot.it
+        )
+
+def fetch(site, noisy=0):
+    """ Fetch either the given URL or the given site """
+    if site.startswith("http"):
+        return fetch_url(site, noisy)
+    else:
+        return fetch_smpl(site, noisy)
+
+def fetch_url(url, noisy=0):
+    """ Fetch the given URL """
+    parsed = urlparse.urlsplit(url)
+    return fetch(parsed[1], noisy)
+
+def fetch_smpl(site, noisy=0):
+    """ Fetch RSS feeds of a website """
+
+    for path in PATHS:
+        logging.info("subr_rss: try with %s for %s", path, site)
+
+        result = subr_http.fetch(site, path, noisy=noisy)
+        if not result:
+            continue
+        response, body = result
+
+        encoding = None
+        ctype = response.getheader("Content-Type")
+        if not ctype:
+            logging.warning("subr_rss: no content-type")
+            continue
+
+        ctype, encoding = subr_http.parse_content_type(ctype)
+        if (
+            ctype != "application/atom+xml" and
+            ctype != "application/rss+xml" and
+            ctype != "text/xml" and
+            ctype != "application/xml"
+           ):
+            logging.warning("subr_rss: bad content type: %s", ctype)
+            continue
+
+        #
+        # Here we MUST NOT decode the body, because the XML reader tries
+        # to decode the body before processing it.
+        #
+        #if encoding:
+        #    body = body.decode(encoding)
+
+        return body
+
+    logging.error("subr_rss: can't fetch RSS for %s", site)
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+    if len(sys.argv) == 2:
+        fetch(sys.argv[1], noisy=1)
+        sys.exit(0)
+    sys.stderr.write("usage: subr_rss site\n")
+    sys.exit(1)
