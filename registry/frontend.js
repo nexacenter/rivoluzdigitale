5ab9e5f78752bd9c72190e4093274873a0cddab8
Simone Basso <bassosimone@gmail.com>
2014-04-01 18:44:08 +0200

    Take periodic data snapshots using git

--- registry/frontend.js
+++ registry/frontend.js
@@ -1,7 +1,10 @@
+var git = require("./git");
 var mailer = require("./mailer.js");
 var mainServer = require("./server.js");
 var mainRouter = require("./router.js");
 var signup = require("./signup.js");
+
+git.start();
 
 mailer.init(function() {
     //mainServer.start(signup.handleRequest);
