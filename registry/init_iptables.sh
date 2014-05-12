6fb3b59de4484396a245ceef619892c96b18c2f7
alemela <alessiom92@gmail.com>
2014-05-12 11:18:16 +0200

    Soft redirect from http to https

--- registry/init_iptables.sh
+++ registry/init_iptables.sh
@@ -35,5 +35,11 @@ SUDO=/usr/bin/sudo
 SOURCE=443
 DEST=4443
 
+SOURCE2=80
+DEST2=8080
+
 $SUDO $IPTABLES -t nat -A PREROUTING -p tcp --dport $SOURCE \
     -j REDIRECT --to-port $DEST
+
+$SUDO $IPTABLES -t nat -A PREROUTING -p tcp --dport $SOURCE2 \
+    -j REDIRECT --to-port $DEST2
