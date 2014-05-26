nexacenter/rivoluzdigitale

commit a07f33b7f0c358ac243d5cb4f5fafa1e3f8fb7a8

rivoluz: we now change dir in run.sh

registry/rivoluz.c
@@ -39,7 +39,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define DEPLOYDIR "/usr/local/share/rivoluz/"
 #define LOGFILE "/var/lib/rivoluz/log.txt"
 #define UNPRIV_USER "_rivoluz"
 
@@ -82,14 +81,6 @@ main(void)
 	    credentials->pw_uid);
 	if (result != 0)
 		err(1, "setresuid");
-
-	/*
-	 * Change directory
-	 */
-
-	result = chdir(DEPLOYDIR);
-	if (result != 0)
-		err(1, "chdir");
 
 	/*
 	 * Become a daemon

commit b741e9506ffefe14760c956843b767a7bec8e046

rivoluz.c: fix regression introduced by a07f33b7...
Basically, because we don't chdir() anymore, we must execute
/usr/local/share/rivoluz/run.sh rather than ./run.sh.

registry/rivoluz.c
@@ -50,7 +50,7 @@ main(void)
 	int result;
 
 	char *const argv[] = {
-		"./run.sh",
+		"/usr/local/share/rivoluz/run.sh",
 		NULL
 	};
 	char *const envp[] = {